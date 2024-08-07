ad91acff4fe8cef9d0e47236a95abc8d85d0c194
sumalee023 <60998336+sumalee023@users.noreply.github.com>
2024-08-07 14:01:22 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const WebSocket = require('ws');
+const app = express();
+const wss = new WebSocket.Server({ port: 9988 });
+wss.on('connection', (ws) => {
+  console.log('A new client connected.');
+  ws.on('message', (message) => {
+    console.log('Received message:', message.toString());
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message.toString());
+      }
+    });
+  });
+  ws.on('close', () => {
+    console.log('A client disconnected.');
+  });
+});
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`);
+});
+
